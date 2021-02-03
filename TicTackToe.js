43ae6038923fa82855bc4eea69c392761ded1559
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2021-02-02 23:53:30 -0800

    Add files via upload

---
+++ TicTackToe.js
@@ -0,0 +1,34 @@
+// resatrt game button 
+var restart = document.querySelector("#button");
+
+// grab all the squares
+var squares = document.querySelectorAll("td");
+
+// clear all the sqaures
+
+function clearBoard(){
+    for (var i=0;i<squares.length;i++){
+        squares[i].textContent = '';
+    }
+}
+
+restart.addEventListener("click", clearBoard);
+// check the square marker
+function changeMarker(){
+    if(this.textContent == ''){
+        this.textContent = 'X';
+    }
+    else if(this.textContent == 'X'){
+        this.textContent = 'O';
+    }
+    else{
+        this.textContent = '';
+    }
+}
+
+for(var i=0;i<squares.length; i++){
+    squares[i].addEventListener('click', changeMarker)
+}
+
+
+// for loop to add event listeners to all the sqaures
